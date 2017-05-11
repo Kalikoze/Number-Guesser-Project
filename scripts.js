4e44ec7efe3371bbc58cdbfe82fc57fda54cedca
Travis Rollins <TROLL59910@aol.com>
2017-05-11 09:29:41 -0600

    Min and max now only accept numbers

--- scripts.js
+++ scripts.js
@@ -123,22 +123,3 @@ function boom () {
 
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function range (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-} */
